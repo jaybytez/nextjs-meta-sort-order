d450969cdd3b9f0ba8135dc95f35524d3a8a50e8
Jay Blanton <jay.blanton@oracle.com>
2020-09-30 09:26:22 -0700

    Change links and tags.

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
 function About() {
-    return (
-      <>
-        <Head>
-          <meta name='test' content='test2'></meta>  
-          <meta name='fly' content='fly2'></meta>
-        </Head>
-        <div>Welcome to Next.js! <Link href='/home'><a>home</a></Link>  </div>
-        
-      </>
-    )
+  return (
+    <>
+      <Head>
+        <meta name='test' content='test2'></meta>
+        <meta name='pageName' content='about'></meta>
+      </Head>
+      <div>Welcome to Next.js! <p> Click here -&gt; <Link href='/home'><a>Home</a></Link>  </p></div>
+
+    </>
+  )
 }
-  
-  export default About
+
+export default About
--- pages/home.js
+++ pages/home.js
@@ -1,17 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
 function HomePage() {
-    return (
-      <>
-        <Head>
-          <meta name='test' content='test2'></meta>
-          <meta name='nochange' content='nochange'></meta>
-        </Head>
-        <div>Welcome to Next.js! <Link href='/about'><a>About</a></Link> </div>
-        
+  return (
+    <>
+      <Head>
+        <meta name='test' content='test2'></meta>
+        <meta name='pageName' content='home'></meta>
+      </Head>
+      <div>Welcome to Next.js! <p> Click here -&gt; <Link href='/about'><a>About</a></Link>  </p></div>
 
-      </>
-    )
+    </>
+  )
 }
-  
-  export default HomePage
+
+export default HomePage
